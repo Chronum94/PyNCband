789d4a857d28f267aab6fbc7c8730f4cb8990e64
Chronum <jinxhaldar@gmail.com>
2018-07-06 00:54:33 -0400

    Define a CoreShellParticle class. Reformat code with black. Let's go.

---
+++ PyNCband/CoreShellParticle.py
@@ -0,0 +1,15 @@
+from .Material import Material
+
+
+class CoreShellParticle:
+    def __init__(
+        self,
+        core_material: Material,
+        shell_material: Material,
+        core_thickness: float,
+        shell_thickness: float,
+    ):
+        self.cmat = core_material
+        self.smat = shell_material
+        self.core_width = core_thickness
+        self.shell_width = shell_thickness
--- PyNCband/Material.py
+++ PyNCband/Material.py
@@ -1,18 +1,25 @@
 """This file contains a simple material class, containing only the properties of the material that we need."""
 
+
 class Material:
-    def __init__(self, bandgap, conduction_band_edge, epsilon, name = None):
+    def __init__(
+        self,
+        bandgap: float,
+        conduction_band_edge: float,
+        epsilon: float,
+        name: str = None,
+    ):
         """Create a simple material with a positive bandgap in eV, and a negative conduction band edge in eV.
         The epsilon is necessary for Coulomb interaction calculations."""
         self.bandgap = bandgap
-        self.cbe = - conduction_band_edge
-        self.vbe = - conduction_band_edge - bandgap
+        self.cbe = -conduction_band_edge
+        self.vbe = -conduction_band_edge - bandgap
         self.eps = epsilon
         if name != None:
             self.name = name
         else:
             self.name = None
-    `
+
     def __str__(self):
         if self.name != None:
             return self.name
