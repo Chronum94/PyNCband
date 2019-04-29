76cf43390c0e9f1714aed370d46ad54614c90e8b
Chronum <jinxhaldar@gmail.com>
2019-04-28 22:56:16 -0400

    Last update for this iteration. Going to switch to a different interface.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyncband",
-    version="0.11.4",
+    version="0.11.5",
     packages=["pyncband"],
     url="",
     license="MIT",
@@ -10,8 +10,8 @@ setup(
     author_email="",
     description="A 1st-order quantum dot bandgap calculator",
     install_requires=[
-        "matplotlib>=2.0.0",
-        "numpy>=1.14.5",
+        "matplotlib>=3.0.0",
+        "numpy>=1.16.0",
         "numba>=0.39.0",
         "scipy>=1.1.0",
         "typing>=3.6.4",
