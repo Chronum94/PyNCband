5c417c4c091f8cb1cbad0e2dbee8c267a96d19d4
Chronum <jinxhaldar@gmail.com>
2019-11-09 12:49:52 -0500

    Some non-changes to start off with literature-comparisons.

--- tests/csp2tests.py
+++ tests/csp2tests.py
@@ -21,14 +21,14 @@ def test_wavefunction_and_derivative_continuity():
 
 
 def test_s1_energies():
-    a = Material2(1.0, -2, 0.018, 0.05, 1.0)
-    b = Material2(1.1, -1.5, 0.09, 0.11, 1.0)
+    a = Material2(1.0, -2, 0.1, 0.6, 1.0)
+    b = Material2(1.1, -1.5, 0.1, 0.6, 1.0)
 
-    core_radius, shell_thickness = 60, 60
+    # core_radius, shell_thickness = 60, 60
     cs = CoreShellParticle2(a, b)
 
     npts = 100
-    r = np.linspace(20, 300, npts)
+    r = np.linspace(10, 300, npts)
     corex, shellx = np.meshgrid(r, r)
     s1_energies = np.zeros_like(corex)
 
@@ -36,7 +36,7 @@ def test_s1_energies():
         for j in range(npts):
             s1_energies[i, j] = np.sum(cs._calculate_s1_energies(corex[i, j], shellx[i,j]))
 
-    plt.contourf(corex / 18.8973, shellx / 18.8973, s1_energies, levels = np.linspace(0, 0.25, 50), extend='both')
+    plt.contourf(corex / 18.8973, shellx / 18.8973, np.log(s1_energies), levels=20)
     plt.colorbar()
     plt.show()
 
